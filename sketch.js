d548d2fe80ae8d7b8b5d854eb3adef26a11f8b72
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2024-01-30 14:29:34 +0000

    change to wwhite

--- sketch.js
+++ sketch.js
@@ -4,53 +4,54 @@ function setup() {
     //#f3f3f3 - white
     //#f50f0f - red
     //#fae317 - yellow
-    //#000000 - yellow
+    //#000000 - black
     //#96#0c84be - blue
 
-    const possibleColors = ["#f3f3f3", "#f50f0f", "#fae317", "#000000", "#96#0c84be"];
+    const possibleColors = ["#f3f3f3", "#f50f0f", "#fae317", "#000000", "#1e81b0"];
 
     //size of each square in pixels
-    const squareSize = 100;
+    const squareSize = 50;
     //number of cells of the gric
     const cellNumber = 10;
     //canvas size in pixels
     const canvasSize = squareSize * cellNumber;
 
+    const width = squareSize;
+    const height = squareSize;
+
     createCanvas(canvasSize, canvasSize);
     background(220);
 
-    for(let row = 0; row < cellNumber; row++) {
-        for(let column = 0; column < cellNumber; column++) {
-            const red = random(255);
-            const green = random(255);
-            const blue = random(255);
+    let x = 0;
+    let y = 0;
 
 
+    while(y < canvasSize) {
+        x = 0;
+
+        while(x < canvasSize) {
+
+            //choose random hex code from all possible colors
             const colorCode = random(possibleColors);
 
+            // store color object in myColor
             const myColor = color(colorCode);
 
             // use fill to fill all the square with one color
             fill(myColor)
 
-            //calculate horizontal position in pixels
-            const x = row * squareSize;
-            //calculate the vertical position in pixels;
-            const y = column * squareSize;
-
-            //set witdh  and height
-            const width = squareSize;
-            const height = squareSize;
 
             //rect(x, y, width, height)
             //x = position of the left of the square
             //y = position of the top og the square
             //width = size of the width of the rectangle
             //horizontal = size of the horizantol of the rectangle
-            square(x, y, width);
+            rect(x, y, width, height);
 
-            
+          x = x + width;
+
         }
+        y = y + height;
     }
 
 }
